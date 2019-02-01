afe1e2fbe9d2f6c8d4cffdc9944688b591313367
Axel Cervantes <axl@Axltosh.local>
2019-01-31 22:19:47 -0600

    conflictos solucionados

--- GuiaUnam-master/GuiaUnam/AppDelegate.swift
+++ GuiaUnam-master/GuiaUnam/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Thread.sleep(forTimeInterval: 3.0)
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         let containerViewController = ContainerViewController()
     
